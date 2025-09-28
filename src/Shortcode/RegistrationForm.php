b5600338796f45f9b1c52db14b7885af26a1d121
Fabien Heilles <phabi1@hotmail.fr>
2025-09-28 06:26:31 +0200

    update plugin

--- src/Shortcode/RegistrationForm.php
+++ src/Shortcode/RegistrationForm.php
@@ -15,6 +15,9 @@ class RegistrationForm extends \Wolf\Core\Shortcode
             return '';
         }
 
+        $info = include_once ABSPATH . 'wp-content/plugins/wolf-events/dist/event-registration-form/main.asset.php';
+        wp_enqueue_script('event-registration-form', plugins_url('../../dist/event-registration-form/main.js', __FILE__), array_merge(['wp-element', 'jquery'], $info['dependencies']), $info['version'], true);
+
         $view = new \Wolf\Core\View();
         return $view->render(__DIR__ . '/../../templates/shortcodes/registration-form.html.php', [
             'event' => $event,
--- templates/shortcodes/registration-form.html.php
+++ templates/shortcodes/registration-form.html.php
@@ -1,5 +1,1 @@
      <div id="app"></div>
-     <?php
-        $info = include_once ABSPATH . 'wp-content/plugins/wolf-events/dist/event-registration-form.asset.php';
-        wp_enqueue_script('event-registration-form', plugins_url('../../dist/event-registration-form.js', __FILE__), array_merge(['wp-element', 'jquery'], $info['dependencies']), $info['version'], true);
-        ?>
